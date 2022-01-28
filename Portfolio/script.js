f5fa5686fa25f62d5b1ffe95d831382dbdb77659
Stuti Shrestha <shrestha.stuti@gmail.com>
2022-01-28 10:04:50 +0545

    changes

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -14,3 +14,24 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 }); 
+
+
+// for contact and the icon of title
+    function fun() {
+        alert ("Thank You for you response");
+    }
+    var d = new Date();
+    var s= 0;
+    var m=0;
+    var h=0;
+    var count = 2;
+    setInterval(function() {
+        d = new Date();
+        // m=d.getMilliseconds();
+        if (count<=43){
+            document.getElementById("icon").href = "icon/apngframe"+count+".png";
+            count=count+1;
+        }else{count=1;}
+        // document.title =m;
+    },23.255814)
+    
